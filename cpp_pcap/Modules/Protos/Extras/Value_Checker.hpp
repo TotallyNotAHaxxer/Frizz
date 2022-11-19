8c76b576c212e04265f9844e02aedbb12587d8a5
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2022-11-18 19:11:55 -0500

    Delete cpp_pcap directory

--- cpp_pcap/Modules/Protos/Extras/Value_Checker.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once 
-#include <iostream>
-#include <algorithm>
-
-namespace Value_Container {
-    template<class C, typename T> bool Checker(C&& c, T t) { return std::find(std::begin(c), std::end(c), t) != std::end(c); }
-};
