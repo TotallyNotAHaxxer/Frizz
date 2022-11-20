9ea426f8e2ec63382e4458df8dc5db72f68b76a3
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2022-11-19 22:42:14 -0500

    Delete Modules directory

--- Modules/Gsrc/Generator/Engine/PreLoaders.go
+++
@@ -1,8 +0,0 @@
-package Engine
-
-import Helpers "main/Modules/Gsrc/Helpers"
-
-// Before loading we want to check all of the arrays for duplicate information
-func Preloader(Vectocheck []string) {
-	Vectocheck = Helpers.ValueRemover(Vectocheck)
-}
